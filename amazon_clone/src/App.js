06e32e85490da7daf55f9df62f52f5e6b6cecf39
jblack016 <j.black16@icloud.com>
2022-12-21 15:54:00 -0600

    Initial Commit

---
+++ amazon_clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SearchIcon from '@mui/icons-material/Search';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
